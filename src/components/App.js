a424cac049c77b4082b638e842ddafc670729e24
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-09 11:28:54 -0600

    notworkingdbjson

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  
 
+  
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
